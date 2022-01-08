a07e5db8eec58b1170fd8816159dbdefcf52b181
Christopher Sardegna <gpg@reagentx.net>
2022-01-08 16:57:01 -0700

    Add table name consts

--- src/tables/table.rs
+++ src/tables/table.rs
@@ -1,11 +1,15 @@
-use rusqlite::{Row, Result, Connection, Statement};
+use rusqlite::{Connection, Result, Row, Statement};
 
 pub trait Table {
-    fn from_row(row: &Row) -> Result<Self> where Self: Sized;
+    fn from_row(row: &Row) -> Result<Self>
+    where
+        Self: Sized;
     fn get(db: &Connection) -> Statement;
 }
 
 pub const HANDLE: &str = "handle";
 pub const MESSAGE: &str = "message";
 pub const CHAT: &str = "chat";
+pub const CHAT_MESSAGE_JOIN: &str = "chat_message_join";
+pub const CHAT_HANDLE_JOIN: &str = "chat_handle_join";
 pub const ME: &str = "Me";
