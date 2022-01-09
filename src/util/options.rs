32720155ab3faac4c8b61044002f0a52798b888b
Christopher Sardegna <gpg@reagentx.net>
2022-01-08 17:07:26 -0700

    Add CLI arg parsing

---
+++ src/util/options.rs
@@ -0,0 +1,23 @@
+use clap::{App, Arg, ArgMatches};
+
+pub fn from_command_line() -> ArgMatches {
+    let matches = App::new("iMessage Exporter")
+        .version("")
+        .about("")
+        .arg(
+            Arg::new("db-path")
+                .short('d')
+                .long("db-path")
+                .help("Specify a custom path for the iMessage databse file")
+                .takes_value(true)
+                .value_name("stream"),
+        )
+        .arg(
+            Arg::new("no-copy")
+                .short('n')
+                .long("no-copy")
+                .help("Do not copy attachments, instead reference them in-place"),
+        )
+        .get_matches();
+    matches
+}
