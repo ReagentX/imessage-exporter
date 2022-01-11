5275e38bfd128346205587c7ea336859d232480a
Christopher Sardegna <gpg@reagentx.net>
2022-01-10 23:55:49 -0500

    Use consts for most strings

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 mod tables;
 mod util;
 
-use util::options::from_command_line;
+use {
+    tables::table::{DEFAULT_PATH, OPTION_COPY, OPTION_PATH},
+    util::options::from_command_line,
+};
 
 fn main() {
+    // Get options from command line
     let options = from_command_line();
-    let user_path = options.value_of("db-path");
-    let no_copy = options.is_present("no-copy");
+    let user_path = options.value_of(OPTION_PATH);
+    let no_copy = options.is_present(OPTION_COPY);
 
-    let db_path = user_path.unwrap_or("/Users/chris/Library/Messages/chat.db");
+    // Get the local database connection string
+    let db_path = user_path.unwrap_or(DEFAULT_PATH);
 
     // Create app state and runtime
     let app = util::runtime::State::new(db_path.to_owned(), no_copy).unwrap();
+
+    // Run some app methods
     app.iter_threads();
     app.iter_messages();
 }
--- src/util/options.rs
+++ src/util/options.rs
@@ -1,21 +1,23 @@
 use clap::{App, Arg, ArgMatches};
 
+use crate::tables::table::{OPTION_COPY, OPTION_PATH};
+
 pub fn from_command_line() -> ArgMatches {
     let matches = App::new("iMessage Exporter")
         .version("")
         .about("")
         .arg(
-            Arg::new("db-path")
+            Arg::new(OPTION_PATH)
                 .short('d')
-                .long("db-path")
+                .long(OPTION_PATH)
                 .help("Specify a custom path for the iMessage databse file")
                 .takes_value(true)
                 .value_name("path/to/chat.db"),
         )
         .arg(
-            Arg::new("no-copy")
+            Arg::new(OPTION_COPY)
                 .short('n')
-                .long("no-copy")
+                .long(OPTION_COPY)
                 .help("Do not copy attachments, instead reference them in-place"),
         )
         .get_matches();
