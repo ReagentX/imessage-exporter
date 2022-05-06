beeae7a6f8a067b7841c61df66c807c5b0ee776a
Christopher Sardegna <gpg@reagentx.net>
2022-05-05 22:27:16 -0400

    Add unknown variant

---
+++ imessage-database/src/message_types/unknown.rs
@@ -0,0 +1,19 @@
+use std::fmt::Display;
+
+use crate::Variant;
+
+/// Apple Pay Requests
+#[derive(Debug, Clone, Copy)]
+pub enum Unknown {
+    Unknown(i32),
+}
+
+impl Variant for Unknown {}
+
+impl Display for Unknown {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Unknown::Unknown(id) => write!(f, "Unkown message type: {}", id),
+        }
+    }
+}
