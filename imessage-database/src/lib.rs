d226cce7c1265cfdced6f20f38bf161b2b0e9172
Christopher Sardegna <gpg@reagentx.net>
2022-05-05 22:26:50 -0400

    Add some exports

--- imessage-database/src/lib.rs
+++ imessage-database/src/lib.rs
@@ -1,3 +1,11 @@
+pub mod message_types;
 pub mod tables;
 pub mod util;
-pub mod message;
+
+pub use {
+    message_types::{applepay::ApplePay, reactions::Reaction, variants::{Variant, get_variant}, unknown::Unknown},
+    tables::{
+        attachment::Attachment, chat::Chat, chat_handle::ChatToHandle, handle::Handle,
+        messages::Message, table::Table,
+    },
+};
