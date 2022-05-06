216318f607815da598e1a5579ca718d34c2f9d3a
Christopher Sardegna <gpg@reagentx.net>
2022-05-05 22:43:10 -0400

    Use apple pay text string

--- imessage-database/src/message_types/applepay.rs
+++ imessage-database/src/message_types/applepay.rs
@@ -3,11 +3,11 @@ use std::fmt::Display;
 use crate::Variant;
 
 /// Apple Pay Requests
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub enum ApplePay {
-    Request(i32), // Unused currently since this uses associated_message_type == 0
-    Send(i32),
-    Recieve(i32),
+    Request(String), // Unused currently since this uses associated_message_type == 0
+    Send(String),
+    Recieve(String),
 }
 
 impl Variant for ApplePay {}
@@ -15,9 +15,9 @@ impl Variant for ApplePay {}
 impl Display for ApplePay {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ApplePay::Request(amount) => write!(f, "Requested Apple Pay"),
-            ApplePay::Send(amount) => write!(f, "Sent Apple Pay"),
-            ApplePay::Recieve(amount) => write!(f, "Recieved Apple Pay"),
+            ApplePay::Request(amount) => write!(f, "{amount}"),
+            ApplePay::Send(amount) => write!(f, "{amount}"),
+            ApplePay::Recieve(amount) => write!(f, "{amount}"),
         }
     }
 }
