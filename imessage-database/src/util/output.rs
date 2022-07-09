435d20bd122604215d1772308adb77271de0dc65
Christopher Sardegna <github@reagentx.net>
2022-07-09 14:22:38 -0400

    Fix console overwrite bug

--- imessage-database/src/util/output.rs
+++ imessage-database/src/util/output.rs
@@ -4,18 +4,32 @@
 
 use std::io::{stdout, Write};
 
-
 /// Write to the CLI while something is working so that we can overwrite it later
-/// 
+///
 /// # Example:
-/// 
+///
 /// ```
 /// use imessage_database::util::output::processing;
-/// 
+///
 /// processing();
 /// println!("Done working!");
 /// ```
 pub fn processing() {
     print!("\rProcessing...");
+    stdout().flush().unwrap();
+}
+
+/// Overwrite the CLI when something is done working so that we can write cleanly later
+///
+/// # Example:
+///
+/// ```
+/// use imessage_database::util::output::{processing, done_processing};
+///
+/// processing();
+/// done_processing();
+/// ```
+pub fn done_processing() {
+    print!("\r");
     stdout().flush().unwrap();
 }
