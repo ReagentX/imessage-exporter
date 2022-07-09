bd4e6772554691db6bd83d3219acc7ee6d79d263
Christopher Sardegna <github@reagentx.net>
2022-07-09 14:22:15 -0400

    Remove unused constructor

--- imessage-exporter/src/app/progress.rs
+++ imessage-exporter/src/app/progress.rs
@@ -9,13 +9,3 @@ pub fn build_progress_bar_export(total_messages: u64) -> ProgressBar {
     pb.set_position(0);
     pb
 }
-
-pub fn build_progress_bar_diagnostic(total_messages: u64) -> ProgressBar {
-    let pb = ProgressBar::new(total_messages);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed}] [{bar:.blue}] {pos}/{len} ({per_sec}, ETA: {eta})")
-        .progress_chars("#>-")
-    );
-    pb.set_position(0);
-    pb
-}
