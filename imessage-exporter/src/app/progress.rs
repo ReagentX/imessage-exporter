4ffc130301674e5746e635e601ec13316fde2c48
Christopher Sardegna <github@reagentx.net>
2022-07-09 11:08:07 -0400

    Update progress bar syle

--- imessage-exporter/src/app/progress.rs
+++ imessage-exporter/src/app/progress.rs
@@ -3,7 +3,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 pub fn build_progress_bar(total_messages: u64) -> ProgressBar {
     let pb = ProgressBar::new(total_messages);
     pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed}] [{bar:.blue}] {pos}/{len} {percent}% ({per_sec}, {eta})")
+        .template("{spinner:.green} [{elapsed}] [{bar:.blue}] {pos}/{len} ({per_sec}, ETA: {eta})")
         .progress_chars("#>-")
     );
     pb.set_position(0);
