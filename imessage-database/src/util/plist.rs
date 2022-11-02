6b6a1ae9c329bfdc8ddc96268a0dcfaca9ab9ace
Christopher Sardegna <github@reagentx.net>
2022-11-01 17:14:58 -0700

    rewrite logic for NSKeyedArchiver parser

--- imessage-database/src/util/plist.rs
+++ imessage-database/src/util/plist.rs
@@ -4,69 +4,130 @@ use plist::{Dictionary, Value};
 
 use std::collections::HashMap;
 
-/// Determine if a plist item is a nested type, currently limited to Dictionaries
-fn is_nested(item: &Value) -> bool {
-    matches!(item, Value::Dictionary(_))
-}
+/// Serialize a message's `payload_data` BLOB from the `NSKeyedArchiver` format to a native Dictionary
+/// that follows the references in the XML document's UID pointers. First, we find the root of the
+/// document, then walk the structure, promoting the end values to the places where their pointers are stored.
+///
+/// For example, a document with a root pointing to some XML like
+///
+/// ```xml
+/// <array>
+///     <dict>
+///         <key>link</key>
+///         <dict>
+///              <key>CF$UID</key>
+///              <integer>47</integer>
+///         </dict>
+///     </dict>
+///     <string>https://chrissardegna.com</string>
+/// </array>
+/// ```
+///
+/// Will be serialized to a dictionary that looks like:
+///
+/// ```json
+/// {
+///     link: https://chrissardegna.com
+/// }
+/// ```
+///
+/// Some detail on this format is described [here](https://en.wikipedia.org/wiki/Property_list#Serializing_to_plist):
+///
+/// > Internally, `NSKeyedArchiver` somewhat recapitulates the binary plist format by
+/// storing an object table array called $objects in the dictionary. Everything else,
+/// including class information, is referenced by a UID pointer. A $top entry under
+/// the dict points to the top-level object the programmer was meaning to encode.
+pub fn parse_plist<'a>(plist: &'a Value) -> Result<Value, PlistParseError> {
+    let body = plist.as_dictionary().ok_or(PlistParseError::InvalidType(
+        "body".to_string(),
+        "dictionary".to_string(),
+    ))?;
+    let objects = body
+        .get("$objects")
+        .ok_or(PlistParseError::MissingKey("$objects".to_string()))?
+        .as_array()
+        .ok_or(PlistParseError::InvalidType(
+            "$objects".to_string(),
+            "array".to_string(),
+        ))?;
 
-/// Extract the name of a class
-fn get_class_name<'a>(
-    objects: &'a Vec<Value>,
-    item: &'a Dictionary,
-) -> Result<&'a str, PlistParseError<'static>> {
-    // Get the class to determine how to parse the data
-    let class_idx = item
-        .get("$class")
-        .ok_or(PlistParseError::MissingKey("$class"))?
+    // Index of root object
+    let root = body
+        .get("$top")
+        .ok_or(PlistParseError::MissingKey("$top".to_string()))?
+        .as_dictionary()
+        .ok_or(PlistParseError::InvalidType(
+            "$top".to_string(),
+            "dictionary".to_string(),
+        ))?
+        .get("root")
+        .ok_or(PlistParseError::MissingKey("root".to_string()))?
         .as_uid()
-        .ok_or(PlistParseError::InvalidType("$class", "uid"))?
+        .ok_or(PlistParseError::InvalidType(
+            "root".to_string(),
+            "uid".to_string(),
+        ))?
         .get() as usize;
 
-    let class = objects
-        .get(class_idx)
-        .ok_or(PlistParseError::NoValueAtIndex(class_idx))?
-        .as_dictionary()
-        .ok_or(PlistParseError::InvalidTypeIndex(class_idx, "dictionary"))?;
-
-    let class_name = class
-        .get("$classname")
-        .ok_or(PlistParseError::MissingKey("$classname"))?
-        .as_string()
-        .ok_or(PlistParseError::InvalidType("$classname", "string"))?;
-
-    Ok(class_name)
+    follow_uid(objects, root, None, None)
 }
 
-/// Recursively extract Values from the obejcts array
-fn extract<'a>(
+/// Recursively follows pointers in an `NSKeyedArchiver` format, promoting the values
+/// to the positions where the pointers live
+fn follow_uid<'a>(
     objects: &'a Vec<Value>,
     root: usize,
-    cache: &mut HashMap<&'a str, &'a Value>,
-    key: Option<&'a str>,
-) -> Result<(), PlistParseError<'static>> {
-    // Get the current item, if possible
-    let item = objects
-        .get(root)
-        .ok_or(PlistParseError::NoValueAtIndex(root))?;
+    parent: Option<String>,
+    item: Option<&'a Value>,
+) -> Result<Value, PlistParseError> {
+    let item = match item {
+        Some(item) => item,
+        None => objects
+            .get(root)
+            .ok_or(PlistParseError::NoValueAtIndex(root))?,
+    };
 
-    // If it is nested, determine if the data is in a known layout, otherwise skip
-    if let Some(data) = item.as_dictionary() {
-        // Get the class to determine how to parse the data
-        match get_class_name(objects, data).unwrap_or("") {
-            // These data structures have custom mapping and need their own logic
-            "NSDictionary" | "NSMutableDictionary" => {
-                // These are the keys in the objects list
-                let keys = data
+    match item {
+        Value::Array(arr) => {
+            let mut array = vec![];
+            for item in arr {
+                if let Some(idx) = item.as_uid() {
+                    array.push(follow_uid(
+                        objects,
+                        idx.get() as usize,
+                        parent.to_owned(),
+                        None,
+                    )?);
+                }
+            }
+            return Ok(plist::Value::Array(array));
+        }
+        Value::Dictionary(dict) => {
+            let mut dictionary = Dictionary::new();
+            // Handle where type is a Dictionary that points to another single value
+            if let Some(relative) = dict.get("NS.relative") {
+                if let Some(idx) = relative.as_uid() {
+                    if let Some(p) = &parent {
+                        dictionary.insert(
+                            p.to_string(),
+                            follow_uid(objects, idx.get() as usize, Some(p.to_string()), None)?,
+                        );
+                    }
+                }
+            }
+            // Handle the NSDictionary and NSMutableDictionary types
+            else if dict.contains_key("NS.keys") && dict.contains_key("NS.objects") {
+                let keys = dict
                     .get("NS.keys")
-                    .ok_or(PlistParseError::MissingKey("NS.keys"))?
+                    .ok_or(PlistParseError::MissingKey("NS.keys".to_string()))?
                     .as_array()
-                    .ok_or(PlistParseError::InvalidType("NS.keys", "array"))?;
+                    .ok_or(PlistParseError::InvalidType("NS.keys".to_string(), "array".to_string()))?;
                 // These are the values in the objects list
-                let values = data
+                let values = dict
                     .get("NS.objects")
-                    .ok_or(PlistParseError::MissingKey("NS.objects"))?
+                    .ok_or(PlistParseError::MissingKey("NS.objects".to_string()))?
                     .as_array()
-                    .ok_or(PlistParseError::InvalidType("NS.objects", "array"))?;
+                    .ok_or(PlistParseError::InvalidType("NS.objects".to_string(), "array".to_string()))?;
                 // Die here if the data is invalid
                 if keys.len() != values.len() {
                     return Err(PlistParseError::InvalidDictionarySize(
@@ -80,108 +141,64 @@ fn extract<'a>(
                         .get(idx)
                         .ok_or(PlistParseError::NoValueAtIndex(idx))?
                         .as_uid()
-                        .ok_or(PlistParseError::InvalidTypeIndex(idx, "uid"))?
+                        .ok_or(PlistParseError::InvalidTypeIndex(idx, "uid".to_string()))?
                         .get() as usize;
                     let value_index = values
                         .get(idx)
                         .ok_or(PlistParseError::NoValueAtIndex(idx))?
                         .as_uid()
-                        .ok_or(PlistParseError::InvalidTypeIndex(idx, "uid"))?
+                        .ok_or(PlistParseError::InvalidTypeIndex(idx, "uid".to_string()))?
                         .get() as usize;
 
                     let key = objects
                         .get(key_index)
-                        .ok_or(PlistParseError::NoValueAtIndex(idx))?
+                        .ok_or(PlistParseError::NoValueAtIndex(key_index))?
                         .as_string()
-                        .ok_or(PlistParseError::InvalidTypeIndex(key_index, "string"))?;
+                        .ok_or(PlistParseError::InvalidTypeIndex(key_index, "string".to_string()))?;
                     let value = objects
                         .get(value_index)
-                        .ok_or(PlistParseError::NoValueAtIndex(idx))?;
+                        .ok_or(PlistParseError::NoValueAtIndex(value_index))?;
 
-                    match is_nested(value) {
-                        true => {
-                            extract(objects, value_index, cache, Some(key))?;
-                        }
-                        false => {
-                            cache.insert(key, value);
-                        }
-                    }
+                    dictionary.insert(
+                        key.to_string(),
+                        follow_uid(objects, value_index, Some(key.to_string()), None)?,
+                    );
                 }
             }
-            "NSURL" => {
-                let url_idx = data
-                    .get("NS.relative")
-                    .ok_or(PlistParseError::MissingKey("NS.relative"))?
-                    .as_uid()
-                    .ok_or(PlistParseError::InvalidType("NS.relative", "uid"))?
-                    .get() as usize;
-
-                let url = objects
-                    .get(url_idx)
-                    .ok_or(PlistParseError::NoValueAtIndex(url_idx))?;
-                cache.insert(key.ok_or(PlistParseError::NoValueAtIndex(url_idx))?, url);
-            }
-            // Generic key-value extractor
-            other => {
-                for (key, val) in data.into_iter() {
-                    // This metadata describes the Obj-C or Swift class NSUnarchiver would serailize to
-                    // But since we are supplying our own logic, this is not necessary
+            // Handle a normal `{key: value}` style dictionary
+            else {
+                for (key, val) in dict {
+                    // Handle skips
                     if key == "$class" {
                         continue;
                     }
+                    // If the value is a pointer, follow it
                     if let Some(idx) = val.as_uid() {
-                        // println!("{other} {idx:?} from {key}");
-                        extract(objects, idx.get() as usize, cache, Some(key))?;
-                    } else {
-                        cache.insert(key, val);
+                        dictionary.insert(
+                            key.to_owned(),
+                            follow_uid(objects, idx.get() as usize, Some(key.to_string()), None)?,
+                        );
+                    }
+                    // If the value is not a pointer, try and follow the data itself
+                    else {
+                        if let Some(p) = &parent {
+                            dictionary.insert(
+                                p.to_owned(),
+                                follow_uid(objects, root, Some(p.to_string()), Some(val))?,
+                            );
+                        }
                     }
                 }
             }
+            return Ok(plist::Value::Dictionary(dictionary));
         }
-    } else {
-        if let Some(k) = key {
-            cache.insert(k, item);
+        Value::Uid(uid) => {
+            return follow_uid(objects, uid.get() as usize, None, None);
         }
-    }
-
-    Ok(())
-}
-
-/// Parse a message's `payload_data`
-///
-/// We parse them using the logic described [here](https://en.wikipedia.org/wiki/Property_list#Serializing_to_plist):
-///
-/// > Internally, [NSKeyedArchiver] somewhat recapitulates the binary plist format by
-/// storing an object table array called $objects in the dictionary. Everything else,
-/// including class information, is referenced by a UID pointer. A $top entry under
-/// the dict points to the top-level object the programmer was meaning to encode.
-pub fn parse_plist(plist: &Value) -> Result<HashMap<&str, &Value>, PlistParseError<'static>> {
-    let mut possible_data = HashMap::new();
-
-    let body = plist
-        .as_dictionary()
-        .ok_or(PlistParseError::InvalidType("body", "dictionary"))?;
-    let objects = body
-        .get("$objects")
-        .ok_or(PlistParseError::MissingKey("$objects"))?
-        .as_array()
-        .ok_or(PlistParseError::InvalidType("$objects", "array"))?;
-
-    // Index of root object
-    let root = body
-        .get("$top")
-        .ok_or(PlistParseError::MissingKey("$top"))?
-        .as_dictionary()
-        .ok_or(PlistParseError::InvalidType("$top", "dictionary"))?
-        .get("root")
-        .ok_or(PlistParseError::MissingKey("root"))?
-        .as_uid()
-        .ok_or(PlistParseError::InvalidType("root", "uid"))?
-        .get() as usize;
-
-    extract(objects, root, &mut possible_data, None)?;
-
-    Ok(possible_data)
+        Value::Boolean(_) | Value::Integer(_) | Value::String(_) => return Ok(item.to_owned()),
+        _ => {}
+    };
+    Ok(objects.get(root).unwrap().to_owned())
 }
 
 pub fn extract_parsed_str<'a>(
@@ -191,5 +208,33 @@ pub fn extract_parsed_str<'a>(
     payload
         .get(key)
         .and_then(|item| item.as_string())
+        .filter(|s| !s.is_empty())
+}
+
+/// Extract a string from a key-value pair that looks like {key: String("value")}
+pub fn get_string_from_dict<'a>(payload: &'a Value, key: &'a str) -> Option<&'a str> {
+    payload
+        .as_dictionary()?
+        .get(key)?
+        .as_string()
+        .filter(|s| !s.is_empty())
+}
+
+/// Extract a bool from a key-value pair that looks like {key: true}
+pub fn get_bool_from_dict<'a>(payload: &'a Value, key: &'a str) -> Option<bool> {
+    payload
+        .as_dictionary()?
+        .get(key)?
+        .as_boolean()
+}
+
+/// Extract a string from a key-value pair that looks like {key: {key: String("value")}}
+pub fn get_string_from_nested_dict<'a>(payload: &'a Value, key: &'a str) -> Option<&'a str> {
+    payload
+        .as_dictionary()?
+        .get(key)?
+        .as_dictionary()?
+        .get(key)?
+        .as_string()
         .filter(|s| !s.is_empty())
 }
