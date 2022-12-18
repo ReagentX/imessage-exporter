dde1ff1d095e25001c3c89f4472e1f9fd5d0d30a
Christopher Sardegna <github@reagentx.net>
2022-12-17 20:11:56 -0800

    Add format_edited to Writer trait

--- imessage-exporter/src/exporters/exporter.rs
+++ imessage-exporter/src/exporters/exporter.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use imessage_database::{
     error::plist::PlistParseError,
-    message_types::{app::AppMessage, url::URLMessage, music::MusicMessage},
+    message_types::{app::AppMessage, music::MusicMessage, url::URLMessage},
     Attachment, Message,
 };
 
@@ -25,13 +25,19 @@ pub(super) trait Writer<'a> {
     /// Format an attachment, possibly by reading the disk
     fn format_attachment(&self, msg: &'a mut Attachment) -> Result<String, &'a str>;
     /// Format an app message by parsing some of its fields
-    fn format_app(&self, msg: &'a Message, attachments: &mut Vec<Attachment>) -> Result<String, PlistParseError>;
+    fn format_app(
+        &self,
+        msg: &'a Message,
+        attachments: &mut Vec<Attachment>,
+    ) -> Result<String, PlistParseError>;
     /// Format a reaction (displayed under a message)
     fn format_reaction(&self, msg: &Message) -> Result<String, String>;
     /// Format an expressive message
     fn format_expressive(&self, msg: &'a Message) -> &'a str;
     /// Format an annoucement message
     fn format_annoucement(&self, msg: &'a Message) -> String;
+    /// Format an edited message
+    fn format_edited(&self, msg: &'a Message) -> Result<String, PlistParseError>;
     fn write_to_file(file: &Path, text: &str);
 }
 
@@ -50,5 +56,10 @@ pub(super) trait BalloonFormatter {
     /// Format a Photo Slideshow message
     fn format_slideshow(&self, balloon: &AppMessage) -> String;
     /// Format a generic app, generally third party
-    fn format_generic_app(&self, balloon: &AppMessage, bundle_id: &str, attachments: &mut Vec<Attachment>) -> String;
+    fn format_generic_app(
+        &self,
+        balloon: &AppMessage,
+        bundle_id: &str,
+        attachments: &mut Vec<Attachment>,
+    ) -> String;
 }
