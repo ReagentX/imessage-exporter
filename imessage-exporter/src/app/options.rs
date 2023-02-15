451547496a942a9e713c5a795c632897ea54a900
Christopher Sardegna <github@reagentx.net>
2023-02-14 22:39:20 -0800

    Support ingesting start_date and end_date data

--- imessage-exporter/src/app/options.rs
+++ imessage-exporter/src/app/options.rs
@@ -2,7 +2,10 @@ use clap::{crate_version, Arg, ArgMatches, Command};
 
 use imessage_database::{
     tables::table::DEFAULT_OUTPUT_DIR,
-    util::dirs::{default_db_path, home},
+    util::{
+        dirs::{default_db_path, home},
+        query_context::QueryContext,
+    },
 };
 
 // CLI Arg Names
@@ -11,13 +14,15 @@ pub const OPTION_COPY: &str = "no-copy";
 pub const OPTION_DIAGNOSTIC: &str = "diagnostics";
 pub const OPTION_EXPORT_TYPE: &str = "format";
 pub const OPTION_EXPORT_PATH: &str = "export-path";
+pub const OPTION_START_DATE: &str = "start-date";
+pub const OPTION_END_DATE: &str = "end-date";
 
 // Other CLI Text
 pub const SUPPORTED_FILE_TYPES: &str = "txt, html";
 pub const ABOUT: &str = concat!(
     "The `imessage-exporter` binary exports iMessage data to\n",
-    "`txt` or `html` formats. It can also run\n",
-    "diagnostics to find problems with the iMessage database."
+    "`txt` or `html` formats. It can also run diagnostics\n",
+    "to find problems with the iMessage database."
 );
 
 pub struct Options<'a> {
@@ -31,6 +36,8 @@ pub struct Options<'a> {
     pub export_type: Option<&'a str>,
     /// Where the app will save exported data
     pub export_path: Option<&'a str>,
+    /// Query context describing SQL query filters
+    pub query_context: QueryContext,
     /// Whether the options created are valid or not
     pub valid: bool,
 }
@@ -42,6 +49,8 @@ impl<'a> Options<'a> {
         let diagnostic = args.is_present(OPTION_DIAGNOSTIC);
         let export_type = args.value_of(OPTION_EXPORT_TYPE);
         let export_path = args.value_of(OPTION_EXPORT_PATH);
+        let start_date = args.value_of(OPTION_START_DATE);
+        let end_date = args.value_of(OPTION_END_DATE);
 
         // Validation layer
         let mut valid = true;
@@ -81,12 +90,28 @@ impl<'a> Options<'a> {
             valid = false;
         }
 
+        // Build query context
+        let mut query_context = QueryContext::default();
+        if let Some(start) = start_date {
+            if let Err(why) = query_context.set_start(start) {
+                eprintln!("{why}");
+                valid = false;
+            }
+        }
+        if let Some(end) = end_date {
+            if let Err(why) = query_context.set_end(end) {
+                eprintln!("{why}");
+                valid = false;
+            }
+        }
+
         Options {
             db_path: user_path.unwrap_or(&default_db_path()).to_string(),
             no_copy,
             diagnostic,
             export_type,
             export_path,
+            query_context,
             valid,
         }
     }
@@ -98,32 +123,36 @@ pub fn from_command_line() -> ArgMatches {
         .about(ABOUT)
         .arg_required_else_help(true)
         .arg(
+            Arg::new(OPTION_DIAGNOSTIC)
+            .short('d')
+            .long(OPTION_DIAGNOSTIC)
+            .help("Print diagnostic information and exit")
+            .display_order(0),
+        )
+        .arg(
+            Arg::new(OPTION_EXPORT_TYPE)
+            .short('f')
+            .long(OPTION_EXPORT_TYPE)
+            .help("Specify a single file format to export messages into")
+            .takes_value(true)
+            .display_order(1)
+            .value_name(SUPPORTED_FILE_TYPES),
+        )
+        .arg(
+            Arg::new(OPTION_COPY)
+            .short('n')
+            .long(OPTION_COPY)
+            .help("Do not copy attachments, instead reference them in-place")
+            .display_order(2),
+        )
+        .arg(
             Arg::new(OPTION_PATH)
                 .short('p')
                 .long(OPTION_PATH)
                 .help(&*format!("Specify a custom path for the iMessage database file\nIf omitted, the default directory is {}", default_db_path()))
                 .takes_value(true)
+                .display_order(3)
                 .value_name("path/to/chat.db"),
-        )
-        .arg(
-            Arg::new(OPTION_COPY)
-                .short('n')
-                .long(OPTION_COPY)
-                .help("Do not copy attachments, instead reference them in-place"),
-        )
-        .arg(
-            Arg::new(OPTION_DIAGNOSTIC)
-                .short('d')
-                .long(OPTION_DIAGNOSTIC)
-                .help("Print diagnostic information and exit"),
-        )
-        .arg(
-            Arg::new(OPTION_EXPORT_TYPE)
-                .short('f')
-                .long(OPTION_EXPORT_TYPE)
-                .help("Specify a single file format to export messages into")
-                .takes_value(true)
-                .value_name(SUPPORTED_FILE_TYPES),
         )
         .arg(
             Arg::new(OPTION_EXPORT_PATH)
@@ -131,7 +160,26 @@ pub fn from_command_line() -> ArgMatches {
                 .long(OPTION_EXPORT_PATH)
                 .help(&*format!("Specify a custom directory for outputting exported data\nIf omitted, the default directory is {}/{DEFAULT_OUTPUT_DIR}", home()))
                 .takes_value(true)
+                .display_order(4)
                 .value_name("path/to/save/files"),
+        )
+        .arg(
+            Arg::new(OPTION_START_DATE)
+                .short('s')
+                .long(OPTION_START_DATE)
+                .help("The start date filter. Only messages sent on or after this date will be included")
+                .takes_value(true)
+                .display_order(5)
+                .value_name("YYYY-MM-DD"),
+        )
+        .arg(
+            Arg::new(OPTION_END_DATE)
+                .short('e')
+                .long(OPTION_END_DATE)
+                .help("The end date filter. Only messages sent before or on this date will be included")
+                .takes_value(true)
+                .display_order(6)
+                .value_name("YYYY-MM-DD"),
         )
         .get_matches();
     matches
