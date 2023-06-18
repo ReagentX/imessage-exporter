a505412f65b1441ed7285bbd5fc23c97b1ae48ab
Christopher Sardegna <github@reagentx.net>
2023-06-17 18:15:24 -0600

    Add attachment file manager for #125

---
+++ imessage-exporter/src/app/attachment_manager.rs
@@ -0,0 +1,140 @@
+use std::{
+    fmt::Display,
+    fs::{copy, create_dir_all, metadata},
+    path::{Path, PathBuf},
+};
+
+use filetime::{set_file_times, FileTime};
+use imessage_database::tables::{attachment::Attachment, messages::Message};
+use uuid::Uuid;
+
+use crate::app::{
+    converter::{heic_to_jpeg, Converter},
+    runtime::Config,
+};
+
+pub enum FileManager {
+    /// Do not copy attachments
+    Disabled,
+    /// Copy and convert attachments to more compatible formats using a [crate::app::converter::Converter]
+    Compatible,
+    /// Copy attachments without converting; preserves quality but may not display correctly in all browsers
+    Efficient,
+}
+
+impl FileManager {
+    pub fn from_cli(copy_state: &str) -> Option<Self> {
+        match copy_state.to_lowercase().as_str() {
+            "compatible" => Some(Self::Compatible),
+            "efficient" => Some(Self::Efficient),
+            "disabled" => Some(Self::Disabled),
+            _ => None,
+        }
+    }
+
+    pub fn copy<'a>(
+        &'a self,
+        message: &Message,
+        attachment: &'a mut Attachment,
+        config: &Config,
+    ) -> Result<PathBuf, &'a str> {
+        let attachment_path = attachment
+            .resolved_attachment_path(&config.options.platform, &config.options.db_path)
+            .ok_or(attachment.filename())?;
+
+        let from = Path::new(&attachment_path);
+
+        if !matches!(self, FileManager::Disabled) {
+            if !from.exists() {
+                eprintln!("Attachment not found at specified path: {from:?}");
+                return Err(attachment.filename());
+            }
+
+            let ext = from.extension().ok_or(attachment.filename())?;
+
+            // Create a path to copy the file to
+            let mut to = config.attachment_path();
+
+            // Add the subdirectory
+            let sub_dir = config.conversation_attachment_path(message.chat_id);
+            to.push(sub_dir);
+
+            // Add a random filename
+            to.push(Uuid::new_v4().to_string());
+
+            to.set_extension(ext);
+
+            match self {
+                FileManager::Compatible => match &config.converter {
+                    Some(converter) => {
+                        // Update extension for conversion
+                        to.set_extension("jpg");
+                        Self::copy_convert(from, &to, converter);
+                    }
+                    None => Self::copy_raw(from, &to),
+                },
+                FileManager::Efficient => Self::copy_raw(from, &to),
+                FileManager::Disabled => unreachable!(),
+            };
+
+            // Update file metadata
+            if let Ok(metadata) = metadata(from) {
+                let mtime = match &message.date(&config.offset) {
+                    Ok(date) => {
+                        FileTime::from_unix_time(date.timestamp(), date.timestamp_subsec_nanos())
+                    }
+                    Err(_) => FileTime::from_last_modification_time(&metadata),
+                };
+
+                let atime = FileTime::from_last_access_time(&metadata);
+
+                if let Err(why) = set_file_times(&to, atime, mtime) {
+                    eprintln!("Unable to update {to:?} metadata: {why}")
+                }
+            }
+            return Ok(to);
+        }
+        Ok(from.to_path_buf())
+    }
+
+    fn copy_raw(from: &Path, to: &Path) {
+        // Ensure the directory tree exists
+        if let Some(folder) = to.parent() {
+            if !folder.exists() {
+                if let Err(why) = create_dir_all(folder) {
+                    eprintln!("Unable to create {folder:?}: {why}");
+                }
+            }
+        }
+        if let Err(why) = copy(from, to) {
+            eprintln!("Unable to copy {from:?} to {to:?}: {why}")
+        };
+    }
+
+    fn copy_convert(from: &Path, to: &Path, converter: &Converter) {
+        let ext = from.extension().unwrap_or_default();
+        if ext == "heic" || ext == "HEIC" {
+            if heic_to_jpeg(from, to, converter).is_none() {
+                eprintln!("Unable to convert {from:?}")
+            }
+        } else {
+            Self::copy_raw(from, to);
+        }
+    }
+}
+
+impl Default for FileManager {
+    fn default() -> Self {
+        Self::Compatible
+    }
+}
+
+impl Display for FileManager {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FileManager::Disabled => write!(fmt, "Disabled"),
+            FileManager::Compatible => write!(fmt, "Compatible"),
+            FileManager::Efficient => write!(fmt, "Efficient"),
+        }
+    }
+}
