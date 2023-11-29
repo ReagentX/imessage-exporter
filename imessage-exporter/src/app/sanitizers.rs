298a7bb6c012b83c6c3bc5ba8fa8ad82a2fa9d64
Christopher Sardegna <github@reagentx.net>
2023-11-28 17:16:51 -0800

    Zero-allocation sanitize html

--- imessage-exporter/src/app/sanitizers.rs
+++ imessage-exporter/src/app/sanitizers.rs
@@ -3,6 +3,15 @@ const FILENAME_REPLACEMENT_CHAR: char = '_';
 /// Characters disallowed in a filename
 const FILENAME_DISALLOWED_CHARS: [char; 3] = ['/', '\\', ':'];
 
+/// The result of possibly sanitizing a string
+#[derive(Debug, PartialEq, Eq)]
+enum SanitizationResult<'a> {
+    /// Used when sanitization allocates a new `String`
+    Edited(String),
+    /// Used when the source does not require sanitization
+    Original(&'a str),
+}
+
 /// Remove unsafe chars in [this list](DISALLOWED_CHARS).
 pub fn sanitize_filename(filename: &str) -> String {
     filename
@@ -18,18 +27,26 @@ pub fn sanitize_filename(filename: &str) -> String {
 }
 
 /// Escapes HTML special characters in the input string.
-pub fn sanitize_html(input: &str) -> String {
-    input.chars().fold(String::new(), |mut acc, c| {
-        match c {
-            '<' => acc.push_str("&lt;"),
-            '>' => acc.push_str("&gt;"),
-            '"' => acc.push_str("&quot;"),
-            '’' => acc.push_str("&#39;"),
-            '&' => acc.push_str("&amp;"),
-            _ => acc.push(c),
-        }
-        acc
-    })
+pub fn sanitize_html<'a>(input: &'a str) -> SanitizationResult {
+    if input
+        .chars()
+        .any(|c| matches!(c, '<' | '>' | '"' | '’' | '&'))
+    {
+        let res = input.chars().fold(String::new(), |mut acc, c| {
+            match c {
+                '<' => acc.push_str("&lt;"),
+                '>' => acc.push_str("&gt;"),
+                '"' => acc.push_str("&quot;"),
+                '’' => acc.push_str("&#39;"),
+                '&' => acc.push_str("&amp;"),
+                _ => acc.push(c),
+            }
+            acc
+        });
+        SanitizationResult::Edited(res)
+    } else {
+        SanitizationResult::Original(input)
+    }
 }
 
 #[cfg(test)]
@@ -54,36 +71,42 @@ mod test_filename {
 
 #[cfg(test)]
 mod tests {
-    use crate::app::sanitizers::sanitize_html;
+    use crate::app::sanitizers::{sanitize_html, SanitizationResult};
 
     #[test]
     fn test_escape_html_chars_basic() {
         assert_eq!(
             sanitize_html("<p>Hello, world > HTML</p>"),
-            "&lt;p&gt;Hello, world &gt; HTML&lt;/p&gt;"
+            SanitizationResult::Edited("&lt;p&gt;Hello, world &gt; HTML&lt;/p&gt;".to_string())
         );
     }
 
     #[test]
     fn doesnt_sanitize_empty_string() {
-        assert_eq!(sanitize_html(""), "");
+        assert_eq!(sanitize_html(""), SanitizationResult::Original(""));
     }
 
     #[test]
     fn doesnt_sanitize_no_special_chars() {
-        assert_eq!(sanitize_html("Hello world"), "Hello world");
+        assert_eq!(
+            sanitize_html("Hello world"),
+            SanitizationResult::Original("Hello world")
+        );
     }
 
     #[test]
     fn can_sanitize_all_special_chars() {
-        assert_eq!(sanitize_html("<>&\"’"), "&lt;&gt;&amp;&quot;&#39;");
+        assert_eq!(
+            sanitize_html("<>&\"’"),
+            SanitizationResult::Edited("&lt;&gt;&amp;&quot;&#39;".to_string())
+        );
     }
 
     #[test]
     fn can_sanitize_mixed_content() {
         assert_eq!(
             sanitize_html("<div>Hello &amp; world</div>"),
-            "&lt;div&gt;Hello &amp;amp; world&lt;/div&gt;"
+            SanitizationResult::Edited("&lt;div&gt;Hello &amp;amp; world&lt;/div&gt;".to_string())
         );
     }
 }
